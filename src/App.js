cd1df7d387e1a7642c5396d29725cafba4e4c035
sugarball <sugarball@foxmail.com>
2017-04-04 22:18:24 +0800

    step 2

--- src/App.js
+++ src/App.js
@@ -3,16 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      msgList: [
+        {
+          msg: 'hello, it\'s me',
+        },
+        {
+          msg: 'i was wondering if after all these years you\'d like to meet',
+        }
+      ],
+      inputMsg: '',
+    }
+  }
+
+  handlePost() {
+    const { inputMsg, msgList } = this.state
+    let _msgList = [...msgList]
+
+    if (!inputMsg) return
+
+    _msgList.push({msg: inputMsg})
+    this.setState({
+      msgList: _msgList,
+      inputMsg: '',
+    })
+    
+  }
+
   render() {
+    const { inputMsg, msgList } = this.state
+    
     return (
       <div className="App">
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to ilabs tech talk</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <section className="input-sec">
+          <input type="text" value={inputMsg} onChange={e => this.setState({inputMsg: e.target.value})} />
+          <button className="post-btn" onClick={this.handlePost.bind(this)}>
+            send
+          </button>
+        </section>
+
+        <div className="msg-sec">
+          {
+            [...msgList].reverse().map((msg, index) => (
+              <div className="msg-content" key={index}>
+                {msg.msg}
+              </div>
+            ))
+          }
+        </div>
       </div>
     );
   }
